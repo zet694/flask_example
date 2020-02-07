f3402d3907c0f876a5743d56ebd390a79b1f73e0
zet694 <vitaliy@particle.one>
2020-02-07 18:53:34 +0300

    Add error for test

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ def validate_post_data(data: dict) -> bool:
 
 @app.route('/', methods=['GET'])
 def hello():
-    return 'Hello World!'
+    # Add error
+    return 'Hello Worlddddddddd!'
 
 
 @app.route('/api', methods=['GET', 'POST'])
